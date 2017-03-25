273a33507a72b52e29a0421e0d777efdc6e10031
mxds-2003 <mxds-2003@mxds2003>
2017-03-24 18:49:54 -0600

    checar lo del loginaction

--- src/java/com/myapp/struts/LoginAction.java
+++ src/java/com/myapp/struts/LoginAction.java
@@ -1,5 +1,12 @@
 package com.myapp.struts;
 
+import java.io.IOException;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
@@ -10,10 +17,31 @@ import org.apache.struts.actions.DispatchAction;
 
 public class LoginAction extends DispatchAction {
 
-	public ActionForward init(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+	public ActionForward init(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws SQLException, ClassNotFoundException, IOException {
 		
 		DynaActionForm frmForma = (DynaActionForm) form;
 		String userAgent = request.getHeader("User-Agent").toLowerCase();
+		String name = (String) frmForma.get("name");
+		String password = (String) frmForma.get("password");
+		
+		Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/prueba", "root", "root");
+		
+           Statement st=con.createStatement();
+           ResultSet rs=st.executeQuery("select * from usuario where name='"+name+"' and password='"+password+"'");
+           int count=0;
+           while(rs.next()){
+           count++;
+          }
+          if(count>0){
+           out.println("welcome "+name);
+           }
+          else{
+           response.sendRedirect("saludo.jsp");
+          }
+        
+    
+		
 		return mapping.findForward("vamos");
 		
 	}
